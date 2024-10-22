f9f7f8c61ba5cc822b6a57da9a01215e1cef1eb5
raptor <raptor@0xdeadbeef.info>
2024-10-22 07:59:25 +0200

    Minor improvements

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-//
-// blindsight - Dump LSASS memory bypassing countermeasures
-// Copyright (c) 2024 Marco Ivaldi <raptor@0xdeadbeef.info>
-//
-// > "There's no such things as survival of the fittest.
-// > Survival of the most adequate, maybe.
-// > It doesn't matter whether a solution's optimal.
-// > All that matters is whether it beats the alternative."
-// >
-// > -- Peter Watts, Blindsight (2006)
-//
-// Red teaming tool to dump LSASS memory, bypassing common countermeasures.
-// It uses Transactional NTFS (TxF API) to transparently scramble the memory
-// dump, to avoid triggering AV/EDR/XDR.
-//
-
 use std::env;
 use std::path::Path;
 use std::process;
@@ -37,14 +21,10 @@ fn main() {
     let action = match args.len() {
         1 => "dump",
         2 => &args[1].clone(),
-        _ => {
-            usage(prog);
-            process::exit(1);
-        }
+        _ => "-",
     };
     if action.starts_with('-') {
         usage(prog);
-        process::exit(1);
     }
 
     // Let's do it
@@ -57,11 +37,13 @@ fn main() {
     }
 }
 
-/// Print usage information
+/// Print usage information and exit
 fn usage(prog: &str) {
     println!("Usage:");
     println!(".\\{prog} [dump | file_to_unscramble.log]");
     println!("\nExamples:");
     println!(".\\{prog}");
     println!(".\\{prog} 29ABE9Hy.log");
+
+    process::exit(1);
 }
